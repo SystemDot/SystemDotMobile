4e296b92edf1793ef3855d405156ef50fb074fe9
sammosampson <sammosampson@gmail.com>
2015-05-04 11:33:19 +0100

    Added ViewModelCommand

---
+++ projects/SystemDot.Mobile/Mvvm/ViewModelCommand.cs
@@ -0,0 +1,86 @@
+namespace SystemDot.Mobile.Mvvm
+{
+    using System;
+    using SystemDot.Core;
+    using Cirrious.MvvmCross.ViewModels;
+
+    public abstract class ViewModelCommand<TViewModel> : MvxCommandBase, IMvxCommand
+        where TViewModel : ViewModel<TViewModel>
+    {
+        public TViewModel ViewModel { get; private set; }
+
+        protected ViewModelCommand(TViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public bool CanExecute()
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+        }
+
+        public void Execute()
+        {
+            OnExecute();
+        }
+
+        protected abstract void OnExecute();
+
+        protected void SendCommandInAsyncContext<TCommand>(Action<TCommand> initaliseCommandAction)
+            where TCommand : new()
+        {
+            ViewModel.SendCommandInAsyncContext(initaliseCommandAction);
+        }
+
+        protected void RaiseInAsyncContext<TEvent>(Action<TEvent> initialiseEvent) where TEvent : new()
+        {
+            ViewModel.RaiseInAsyncContext(initialiseEvent);
+        }
+    }
+
+    public abstract class ViewModelCommand<TParameter, TViewModel> : MvxCommandBase, IMvxCommand
+        where TViewModel : ViewModel<TViewModel>
+    {
+        public TViewModel ViewModel { get; private set; }
+
+        protected ViewModelCommand(TViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public bool CanExecute()
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            OnExecute(parameter.As<TParameter>());
+        }
+
+        public void Execute()
+        {
+        }
+
+        protected abstract void OnExecute(TParameter parameter);
+
+        protected void RaiseInAsyncContext<TEvent>(Action<TEvent> initialiseEvent) where TEvent : new()
+        {
+            ViewModel.RaiseInAsyncContext(initialiseEvent);
+        }
+    }
+}
