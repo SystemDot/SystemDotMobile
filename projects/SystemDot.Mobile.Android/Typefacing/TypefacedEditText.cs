e9be1ab8531449472ab4ea8fe0ffa47ae2934978
sammosampson <sammosampson@gmail.com>
2015-06-15 08:14:16 +0100

    Added typfaced edit test

---
+++ projects/SystemDot.Mobile.Android/Typefacing/TypefacedEditText.cs
@@ -0,0 +1,17 @@
+﻿namespace SystemDot.Mobile.Typefacing
+{
+    using Android.Content;
+    using Android.Util;
+    using Android.Widget;
+
+    public class TypefacedEditText : EditText
+    {
+        // ReSharper disable once MemberCanBeProtected.Global
+        public TypefacedEditText(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            if (IsInEditMode) return;
+
+            Typeface = context.GetTypefaceFromAttributeValue(attrs);
+        }
+    }
+}
